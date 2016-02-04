f660d5e7dc965ab1587ac8f9dec813363f2411a4
VamsiPusapati <vamsi.pusapati2014@gmail.com>
2016-02-03 21:07:06 -0400

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+// main javascript document
+
+"use strict";
+
+var username = "vamsi";
+var age = 22;
+
+var ishungry = true;
+
+var usernames = [];
+
+// initialize the array with values
+usernames = ["Mike", "Rich", "Anju", "Margot"];
+console.log("before the push: " + usernames);
+
+// add an element to the bottom of the Array
+usernames.push("Tom");
+console.log("after the push: " + usernames);
+
+// Remove the element from the bottom of the Array
+usernames.pop();
+console.log("after the pop: " + usernames);
+
+// var usernames = ["Mike", "Rich", "Anju", "Margot"];
+/* var usernames = [];
+// var usernames = new Array();
+
+usernames[0] = "Mike";
+usernames[1] = "Rich";
+usernames[2] = "Anju";
+usernames[3] = "Vamsi";
+console.log(usernames[2]);
+*/
